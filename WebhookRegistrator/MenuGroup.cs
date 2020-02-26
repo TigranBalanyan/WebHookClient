461003b526d64287b9f33098f3ffac6e644f8d7d
Tigran Balanyan <tbalanyan@gmail.com>
2020-02-26 15:59:21 +0400

    Commit

---
+++ WebhookRegistrator/MenuGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebhookRegistrator
+{
+    internal class MenuGroup
+    {
+        public List<MenuGroupWriteModel> Groups { get; set; }
+        public string Language { get; set; }
+    }
+
+    public class MenuGroupWriteModel
+    {
+        public Guid Id { get; set; }
+        public Guid? ParentId { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; } = string.Empty;
+    }
+}
--- WebhookRegistrator/Program.cs
+++ WebhookRegistrator/Program.cs
@@ -1,7 +1,9 @@
 ﻿using IdentityModel.Client;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -11,57 +13,61 @@ namespace WebhookRegistrator
 {
     class Program
     {
-    //    public var _clientScopes = new string[] {
-    //    "openid',
-    //    "offline_access',
-    //    "profile',
-    //    "accounting',
-    //    "virtualcard',
-    //    "client',
-    //    "partner',
-    //    "locations',
-    //    "filestore',
-    //    "userprovider',
-    //    "cardgenerator',
-    //    "advertisement',
-    //    "loyaltyservices',
-    //    "catalog',
-    //    "translation',
-    //    "talkurl',
-    //    "orders',
-    //    "feedback',
-    //    "basket',
-    //    "orders.signalrhub',
-    //    "hotactions',
-    //    "subscriptions',
-    //    "imagecloud',
-    //    "smarttranslate',
-    //    "payment',
-    //    "webhook',
-    //    "webboneeagg'
-    //};
         static void Main(string[] args)
         {
-            var token = RequestTokenFromAuthorizationServer(new Uri("http://localhost:52489/"), "arthar9613@gmail.com", "Mersoft1*").Result;
-            Console.WriteLine(token); //getting authorization token with refresh token
-            
-            //updating authorization token with refresh token
-            var tokenFromRefreshToken = RequestRefreshTokenFromAuthorizationServer(new Uri("http://localhost:52489/"), "arthar9613@gmail.com", "Mersoft1*", "137054b208d5ee245ae1826e622422604952501cc5b3856c6618f899136f3fc5").Result;
-            var token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZCN0FDQzUyMDMwNUJGREI0RjcyNTJEQUVCMjE3N0NDMDkxRkFBRTEiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJhM3JNVWdNRnY5dFBjbExhNnlGM3pBa2ZxdUUifQ.eyJuYmYiOjE1NzkwOTEzODUsImV4cCI6MTU3OTE3Nzc4NSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MjQ4OSIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjUyNDg5L3Jlc291cmNlcyIsImNhdGFsb2ciLCJmaWxlc3RvcmUiXSwiY2xpZW50X2lkIjoidmNtY2xpZW50Iiwic3ViIjoiNGYwYjkwY2EtMWNhYS00ODRkLTg5OTMtNDg4Zjk3MTU5MWVmIiwiYXV0aF90aW1lIjoxNTc5MDkxMzg1LCJpZHAiOiJsb2NhbCIsInByZWZlcnJlZF91c2VybmFtZSI6ImFydGhhcjk2MTNAZ21haWwuY29tIiwidHlwZSI6IjIiLCJ0ZWFtIjoiNGYwYjkwY2EtMWNhYS00ODRkLTg5OTMtNDg4Zjk3MTU5MWVmIiwiZW1haWwiOiJhcnRoYXI5NjEzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJyb2xlIjoiUGFydG5lciIsInNjb3BlIjpbImNhdGFsb2ciLCJmaWxlc3RvcmUiLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.hPMphSdZ3o6slc - DypOdAR8ieMbidl4PvJ38EdL - QSdt6D9RGPULdCiX4JPKK3N0qAfO0XneTFHMbqJLKT - uHutljHYGsGcMkckfJpsqyIIjVCaObsPloF6 - rK0EjK4kuO_gkIZkXwqAZVIpVuKnKOSeOi51o2qyOjZcdNJOwlg1QRZ41XiAgyDvtRdlV - yDQfT4oqPiin72cKsFtRXcM2ZpP7I4_m9HAxF92c4v2uQ2aBYHSVb5tSCR9FbRDFFZ38kpgd5OJr89XG1LVcBySxCs4sKIvz3qnnDboet4RGxvcuXfWlgTHIaf - 9BW8oTzgNbU030dvUwn1jTLuwA1Ig";
+            var tokenEndpoint = "https://d-bonee-identity.azurewebsites.net"; //identity server endpoint
+            var username = ""; //user email
+            var password = ""; //password
+            var authorizationResponse = RequestTokenFromAuthorizationServer(new Uri(tokenEndpoint), username, password).Result;
+            Console.WriteLine(authorizationResponse); //outputing the response
+
+            Console.WriteLine();
+            Console.WriteLine("Getting the access token");
+            var bearerToken = JObject.Parse(authorizationResponse)["access_token"].ToString();
+            Console.WriteLine("access token " + bearerToken);
+
+            Console.WriteLine("Getting the refresh token");
+            var refreshToken = JObject.Parse(authorizationResponse)["refresh_token"].ToString();
+            Console.WriteLine("refresh token " + refreshToken);
+
+            //after authorization token expiration getting the access token again
+            //with refresh token
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var refreshTokenResponse = RequestRefreshTokenFromAuthorizationServer(new Uri(tokenEndpoint), refreshToken).Result;
+            var accessToken = JObject.Parse(refreshTokenResponse)["access_token"].ToString();
+            refreshToken = JObject.Parse(refreshTokenResponse)["refresh_token"].ToString();
+            Console.WriteLine(accessToken);
+            Console.WriteLine();
+            Console.WriteLine(refreshToken);
 
 
             var TranslationsKeys = new List<string>();
             TranslationsKeys.Add("us");
             TranslationsKeys.Add("ru");
+            TranslationsKeys.Add("arm");
 
-            var request = new ClientRequestReadModel()
+            var MenuGroups = new List<MenuGroupWriteModel>
             {
-                ItemId = new Guid("0AB82EF6-D357-48B6-A13C-009F606C2079"),
-                Translations = TranslationsKeys
+                new MenuGroupWriteModel()
+                {
+                    Code = "myCode1",
+                    Id = new Guid(""),
+                    Name = "Group1",
+                    ParentId = new Guid("")
+                }
             };
-            var a = SendRequestMessageToWebhook(request, token);
 
-            Console.WriteLine(a);
+            var request = new MenuGroup()
+            {
+                Language = "en",
+                Groups = MenuGroups
+            };
+
+
+            SendProductRequestMessageToWebhook(request, accessToken);
+
 
             Thread.Sleep(50000);
         }
@@ -80,18 +86,20 @@ namespace WebhookRegistrator
                     {
                         new KeyValuePair<string, string>("grant_type", "password"),
                         new KeyValuePair<string, string>("client_id", "vcmclient"),
-                        new KeyValuePair<string, string>("scope", "filestore catalog offline_access"),
+                        new KeyValuePair<string, string>("scope", "openid iiko offline_access"),
+
+
                         new KeyValuePair<string, string>("username", username),
                         new KeyValuePair<string, string>("password", password)
                     });
-                
+
                 tokenRequest.Content = httpContent;
-                responseMessage =  client.SendAsync(tokenRequest).Result;
+                responseMessage = client.SendAsync(tokenRequest).Result;
             }
             return responseMessage.Content.ReadAsStringAsync().Result;
         }
 
-        public static async Task<string> RequestRefreshTokenFromAuthorizationServer(Uri uriAuthorizationServerUri, string username, string password, string refreshToken)
+        public static async Task<string> RequestRefreshTokenFromAuthorizationServer(Uri uriAuthorizationServerUri, string refreshToken)
         {
             HttpResponseMessage responseMessage = new HttpResponseMessage();
             using (HttpClient client = new HttpClient())
@@ -113,24 +121,26 @@ namespace WebhookRegistrator
             return responseMessage.Content.ReadAsStringAsync().Result;
         }
 
-        public class ClientRequestReadModel
+        public static string SendProductRequestMessageToWebhook(MenuGroup menuGroup, string token)
         {
-            public Guid ItemId { get; set; }
-            public List<string> Translations { get; set; }
-        }
-        public static  string SendMessage(ClientRequestReadModel message, string token)
-        {
-            string uri = "http://localhost:57424/api/v1/WebhookCatalog/GetWebhookProduct";
+            string uri = "https://d-bonee-iiko.azurewebsites.net/api/Request/menugroup";
 
-            HttpResponseMessage responseMessage;
+            HttpResponseMessage responseMessage = null;
             using (HttpClient httpClient = new HttpClient())
             {
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri)
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, uri)
                 {
-                    Content = new StringContent(JsonConvert.SerializeObject(message), UnicodeEncoding.UTF8, "application/json")
+                    Content = new StringContent(JsonConvert.SerializeObject(menuGroup), Encoding.UTF8, "application/json")
                 };
                 request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                responseMessage = httpClient.SendAsync(request).Result;
+                try
+                {
+                    responseMessage = httpClient.SendAsync(request).Result;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception");
+                }
             }
 
             return responseMessage.Content.ReadAsStringAsync().Result;
